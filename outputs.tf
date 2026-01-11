ed5e18facbc950ca75fef2ce833b172738686151
Daniel Allen <dan@aeternagroup.net>
2026-01-11 22:21:39 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "voice_services_communications_gateway_test_lines" {
+  description = "All voice_services_communications_gateway_test_line resources"
+  value       = azurerm_voice_services_communications_gateway_test_line.voice_services_communications_gateway_test_lines
+}
+
